bb806f43b2c0388c13d7b783cf4a3685c5b24417
Mariangithubit <metsyb1@hotmail.com>
2024-01-29 14:41:51 -0800

    Adding CSS & Images

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          'file-loader', 
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true
+            },
+          }
+        ],
+      },
+    ]
+  }
+};
+
