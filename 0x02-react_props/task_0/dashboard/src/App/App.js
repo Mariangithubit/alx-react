ee3571c4b00ce0be93894c3c918f4711a3632afe
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-02-02 22:20:01 +0200

    Update App.js

--- 0x02-react_props/task_0/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -4,11 +4,6 @@ import holberton_logo from './holberton-logo.jpg';
 
 function App() {
   return (
-    <>
-      <div className="App-header">
-        <img src={holberton_logo} alt="logo" />
-        <h1>School dashboard</h1>
-      </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
         <label htmlFor="email">Email: </label>
@@ -17,10 +12,6 @@ function App() {
         <input type="password" id="password" />
         <button>OK</button>
       </div>
-      <div className="App-footer">
-        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-      </div>
-    </>
   );
 }
 
