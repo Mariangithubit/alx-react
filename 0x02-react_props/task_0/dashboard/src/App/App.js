1ade0e60bd00acf4f8969d818d0fcebd0f479e61
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-02-02 22:53:03 +0200

    Update App.js

--- 0x02-react_props/task_0/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -1,11 +1,20 @@
-import './App.css';
-import { getFullYear, getFooterCopy } from './utils';
-import holberton_logo from './holberton-logo.jpg';
+import React from "react";
+import "./App.css";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import Notifications from "../Notifications/Notifications";
 
 function App() {
   return (
     <>
-      <div className="App-body">
+      <Notifications />
+      <div className="App">
+        <Header />
+        <div className="App-body">
+          <Login />
+        </div>
+        <Footer />
       </div>
     </>
   );
