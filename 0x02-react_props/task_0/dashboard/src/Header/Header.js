989358c774d0abfc5c926d92cf10abdd7777c19e
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-02-02 22:13:19 +0200

    Update Header.js

--- 0x02-react_props/task_0/dashboard/src/Header/Header.js
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -9,17 +9,6 @@ function App() {
         <img src={holberton_logo} alt="logo" />
         <h1>School dashboard</h1>
       </div>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-        <label htmlFor="email">Email: </label>
-        <input type="email" id="email" />
-        <label htmlFor="password"> Password: </label>
-        <input type="password" id="password" />
-        <button>OK</button>
-      </div>
-      <div className="App-footer">
-        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-      </div>
     </>
   );
 }
