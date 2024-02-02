69975226aff99909c1899740a3d68dd15797a0d5
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-02-02 22:44:30 +0200

    Update App.js

--- 0x02-react_props/task_0/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -6,12 +6,6 @@ function App() {
   return (
     <>
       <div className="App-body">
-        <p>Login to access the full dashboard</p>
-        <label htmlFor="email">Email: </label>
-        <input type="email" id="email" />
-        <label htmlFor="password"> Password: </label>
-        <input type="password" id="password" />
-        <button>OK</button>
       </div>
     </>
   );
