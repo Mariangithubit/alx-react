6538eda7bcc2efbe802d7cb332d03a5be3d28914
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-02-19 21:58:14 +0200

    Update Footer.js

--- 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
+++ 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
@@ -1,11 +1,25 @@
-import React from "react";
-import './Footer.css';
+import React, { useContext } from "react";
+import "./Footer.css";
 import { getFullYear, getFooterCopy } from "../utils/utils";
+import { AppContext } from "../App/AppContext";
 
-export default function Footer() {
+function Footer() {
+  const { user } = useContext(AppContext);
+
   return (
-    <div className="App-footer">
-      Copyright {getFullYear()} - {getFooterCopy()}
-    </div>
+    <>
+      <div className="App-footer">
+        {user.isLoggedIn && (
+          <p>
+            <a href="#">Contact us</a>
+          </p>
+        )}
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy()}
+        </p>
+      </div>
+    </>
   );
 }
+
+export default Footer;
