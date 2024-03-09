c41d30e02bce858d3d74ec4db06749a534e10a35
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-03-10 00:23:00 +0200

    Update courseReducer.js

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.js
@@ -1,26 +1,21 @@
 import { FETCH_COURSE_SUCCESS, SELECT_COURSE, UNSELECT_COURSE } from "../actions/courseActionTypes";
+import { Map, setIn } from "immutable";
+import { coursesNormalizer } from "../schema/courses";
 
-export function courseReducer (state = [], action) {
+
+const initialState = Map ([])
+export function courseReducer (state = initialState, action) {
   switch(action.type){
     case FETCH_COURSE_SUCCESS: {
-      let data = action.data.map((course)=>({...course}))
-      data.map((course) => course.isSelected = false)
-      return data
+      let courseData = coursesNormalizer(action.data)
+      const courses = courseData.entities.courses
+      courseData.result.map((id)=> courses[id].isSelected = false)
+      return state.merge(courses)
     }
-    case SELECT_COURSE: {
-      const newState = [...state]
-      newState.map((course)=>{
-        if (course.id === action.index) course.isSelected = true
-      })
-      return newState
-    }
-    case UNSELECT_COURSE: {
-      const newState = [...state]
-      newState.map((course)=>{
-        if (course.id === action.index) course.isSelected = false
-      })
-      return newState
-    }
+    case SELECT_COURSE:
+      return setIn(state.toJS(), [action.index, "isSelected"], true)
+    case UNSELECT_COURSE:
+      return setIn(state.toJS(), [action.index, "isSelected"], false)
     default:
       return state
   }
