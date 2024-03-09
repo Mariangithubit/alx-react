a5a1ebcadca26cddd1b554a2c3d4d82232e1e3b4
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-03-10 00:21:45 +0200

    Update notifications.js

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/schema/notifications.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/schema/notifications.js
@@ -1,27 +1,28 @@
-import * as notificationData from "../../../../notifications.json";
+import * as data from '../../notifications.json';
 import { normalize, schema } from "normalizr";
 
-const user = new schema.Entity("users");
-const message = new schema.Entity("messages", {}, { idAttribute: "guid" });
-const notification = new schema.Entity("notification", {
-  author: user,
-  context: message,
-});
-
-const normalized = normalize(notificationData, [notification]);
-
-export default function getAllNotificationsByUser(userId) {
-  const output = [];
-  const notifications = normalized.entities.notification;
-  const messages = normalized.entities.messages;
-
-  for (const id in notifications) {
-    if (notifications[id].author === userId) {
-      output.push(messages[notifications[id].context]);
-    }
-  }
-
-  return output;
+export function notificationsNormalizer(data) {
+  const notification = new schema.Entity("notifications")
+  const normalizedData = normalize(data, [notification])
+  return normalizedData
 }
 
-export { normalized };
+const user = new schema.Entity("users")
+const message = new schema.Entity("messages", {}, {
+  idAttribute: "guid"
+})
+const notification = new schema.Entity("notifications", {
+  author: user,
+  context: message
+})
+
+export const normalizedData = normalize(data.default, [notification])
+// console.log(normalizedData.entities.notifications)
+export const getAllNotificationsByUser = (userId) =>{
+  let userNotifs = []
+  const notifications = normalizedData.entities.notifications
+  const messages = normalizedData.entities.messages
+  const NotifIdList = normalizedData.result.filter((notifId)=>notifications[notifId].author == userId)
+  NotifIdList.forEach((id)=>userNotifs.push(messages[notifications[id].context]))
+  return userNotifs
+}
