0bed09a5cc7e74550e853947fccb9fae4d7befa4
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-03-09 23:44:20 +0200

    Update uiReducer.js

--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
@@ -1,51 +1,29 @@
-import { LOGOUT DISPLAY_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE, HIDE_NOTIFICATION_DRAWER } from '../actions/uiActionTypes';
-// Create the basic state
-// In a file named reducers/uiReducer.js, define the initial state of the reducer for the UI:
-// It should have one boolean property isNotificationDrawerVisible
-// It should have one boolean property isUserLoggedIn
-// It should have one empty object user
-// Create the reducer function
-// In the same file, import all the actions that you created in the file actions/uiActionTypes and create a reducer function named uiReducer:
-// DISPLAY_NOTIFICATION_DRAWER should set isNotificationDrawerVisible to true
-// HIDE_NOTIFICATION_DRAWER should set isNotificationDrawerVisible to false
-// LOGIN_SUCCESS should set isUserLoggedIn to true
-// LOGIN_FAILURE should set isUserLoggedIn to false
-// LOGOUT should set isUserLoggedIn to false
+import { Map } from "immutable";
+import { LOGOUT, DISPLAY_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE, HIDE_NOTIFICATION_DRAWER } from '../actions/uiActionTypes'
 
-export const initialState = () => {
-    isNotificationDrawerVisible: false,
-    isUserLoggedIn: false,
-    user: {}
-}
+// Install Immutable.js within the project
+// Update the uiReducer.js file to use Map from Immutable.js
+// Update the different part of the reducer function to use set from Map
+// Update the test suite, so it takes into account the changes
 
-export default function uiReducer(state = initialState, action) {
-    switch (action.type) {
-        case DISPLAY_NOTIFICATION_DRAWER:
-            return {
-                ...state,
-                isNotificationDrawerVisible: true
-            }
-        case HIDE_NOTIFICATION_DRAWER:
-            return {
-                ...state,
-                isNotificationDrawerVisible: false
-            }
-        case LOGIN_SUCCESS:
-            return {
-                ...state,
-                isUserLoggedIn: true
-            }
-        case LOGIN_FAILURE:
-            return {
-                ..state,
-                isUserLoggedIn: false
-            }
-        case LOGOUT:
-            return {
-                ..state,
-                isUserLoggedIn: false
-            }
-        default:
-            return state
-    }
+export const initialState = Map({
+  isNotificationDrawerVisible: false,
+  isUserLoggedIn: false,
+  user: {}
+})
+
+export function uiReducer(state = initialState, action) {
+  switch(action.type){
+    case DISPLAY_NOTIFICATION_DRAWER:
+      return state.set("isNotificationDrawerVisible", true)
+    case HIDE_NOTIFICATION_DRAWER:
+      return state.set("isNotificationDrawerVisible", false)
+    case LOGIN_SUCCESS:
+      return state.set("isUserLoggedIn", true)
+    case LOGIN_FAILURE: 
+    case LOGOUT:
+      return state.set("isUserLoggedIn", false)
+    default:
+      return state
+  }
 }
