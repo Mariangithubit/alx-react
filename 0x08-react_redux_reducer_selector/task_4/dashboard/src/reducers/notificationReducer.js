6f67c1cd3bb76393d8c64f250937a8c86ab77480
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-03-10 00:24:08 +0200

    Update notificationReducer.js

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
@@ -1,31 +1,25 @@
-import { MARK_AS_READ, SET_TYPE_FILTER, FETCH_NOTIFICATIONS_SUCCESS } from "../actions/notificationActionTypes";
+import { FETCH_NOTIFICATIONS_SUCCESS, MARK_AS_READ, SET_TYPE_FILTER } from "../actions/notificationActionTypes";
+import { Map, setIn } from "immutable";
+import { notificationsNormalizer } from "../schema/notifications";
 
-export const initialState = {
-    notifications: [],
-    filter: "DEFAULT"
-}
+export const initialState = Map({
+  notifications: [],
+  filter: "DEFAULT"
+})
 
-export function notificationReducer(state = initialState, action) {
-    switch(action.type) {
-        case FETCH_NOTIFICATIONS_SUCCESS: {
-            const notifs = action.data.map((item)=> ({...item}))
-            notifs.map((notif) => notif.isRead = false)
-            return {...state, notifications: notifs}
-        }
-        case MARK_AS_READ: {
-            const notifUpdate = [...state.notifications]
-            notifUpdate.map((notif)=> {
-              if (notif.id === action.index) notif.isRead = true
-            })
-            return {...state, notifications: notifUpdate}
-        }
-        case SET_TYPE_FILTER: {
-            return {
-              ...state,
-              filter: action.filter
-            }
-        }
-        default:
-            return state
+export function notificationReducer(state = initialState, action){
+  switch(action.type){
+    case FETCH_NOTIFICATIONS_SUCCESS: {
+      const notifData = notificationsNormalizer(action.data)
+      const notifs = notifData.entities.notifications
+      notifData.result.map((id) => notifs[id].isRead = false)
+      return state.merge(notifData.entities)
     }
+    case MARK_AS_READ:
+      return setIn(state.toJS(), ["notifications", action.index, "isRead"], true)
+    case SET_TYPE_FILTER:
+      return state.set("filter", action.filter)
+    default:
+      return state
+  }
 }
