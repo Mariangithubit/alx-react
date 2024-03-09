5f442f64c30656ac09f0b1095e2c6925e692215f
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-03-10 01:13:02 +0200

    Update index.js
    Create a small store

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,10 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/App';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { uiReducer } from './reducers/uiReducers';
 
+
+const store = createStore(uiReducer)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
