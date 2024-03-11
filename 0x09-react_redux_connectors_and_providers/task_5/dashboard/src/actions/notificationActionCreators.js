3094575788e8bebc05d5aed350a50f8c010bfb4c
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-03-11 23:43:12 +0200

    Update notificationActionCreators.js

--- 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.js
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.js
@@ -1,19 +1,58 @@
-import { NotificationTypeFilters, MARK_AS_READ, SET_TYPE_FILTER } from "./notificationActionTypes";
+import { getAllNotificationsByUser, notificationsNormalizer } from '../schema/notifications'
+import { FETCH_NOTIFICATIONS_SUCCESS, MARK_AS_READ, SET_LOADING_STATE, SET_TYPE_FILTER } from './notificationActionTypes'
 
-export const markAsRead = (index) => {
+export function markAsRead(index) {
+  return { type: MARK_AS_READ, index }
+}
+
+export function setNotificationFilter(filter) {
+  return { type: SET_TYPE_FILTER, filter}
+}
+
+export function fetchNotificationsSucess() {
   return {
-    type: MARK_AS_READ,
-    index,
-  };
-};
+    type: FETCH_NOTIFICATIONS_SUCCESS,
+    data: [
+      {
+        id: 1,
+        type: "default",
+        value: "New course available"
+      },
+      {
+        id: 2,
+        type: "urgent",
+        value: "New resume available"
+      },
+      {
+        id: 3,
+        type: "urgent",
+        value: "New data available"
+      }
+    ]
+  }
+}
 
-export const boundMarkAsRead = (index) => dispatch(markAsRead(index));
+export function setLoadingState(loading){
+  return { type: SET_LOADING_STATE, loading }
+}
 
-export const setNotificationFilter = (filter) => {
-  return {
-    type: SET_TYPE_FILTER,
-    filter,
-  };
-};
+export function setNotifications(data=[]){
+  return { type: FETCH_NOTIFICATIONS_SUCCESS, data }
+}
 
-export const boundSetNotificationFilter = (filter) => dispatch(setNotificationFilter(filter));
+export function fetchNotifications() {
+  return (dispatch) => {
+    dispatch(setLoadingState(true))
+    return fetch('/notifications.json')
+    .then((response) => response.json())
+    .then((data) => {
+      const normalizedData = notificationsNormalizer(data)
+      const messages = normalizedData.entities.messages
+      // console.log(messages)
+      dispatch(setNotifications(messages))
+      dispatch(setLoadingState(false))
+    })
+  }
+}
+// export const dispatchMarkAsRead = (index) => dispatch(markAsRead(index))
+// export const dispatchSetNotificationFilter =(filter) => dispatch(setNotificationFilter(filter))
