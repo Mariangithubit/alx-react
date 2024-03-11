ab3a202914340d2a27ff8e56109aa127344cb74d
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-03-11 21:53:58 +0200

    Connect user state to the Footer

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
@@ -1,25 +1,60 @@
-import React, { useContext } from "react";
-import "./Footer.css";
-import { getFullYear, getFooterCopy } from "../utils/utils";
-import { AppContext } from "../App/AppContext";
+import React from 'react';
+import { StyleSheet, css} from 'aphrodite';
+import { getFullYear, getFooterCopy } from '../utils/utils'
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-function Footer() {
-  const { user } = useContext(AppContext);
 
+const styles = StyleSheet.create({
+  AppFooter: {
+    borderTop: "4px solid #E0354B",
+    width: '99%',
+    '@media (max-width: 900px)': {
+      position: "relative",
+      width: "100%",
+      marginTop: 60
+    }
+  },
+  
+  AppFooter_p: {
+    fontStyle: "italic",
+    textAlign: "center"
+  }
+
+
+})
+
+
+
+export function Footer(props) {
+  const footerText = (props.isLoggedIn) ? <a href="#">Contact us</a> : `Copyright ${getFullYear()} - ${getFooterCopy(false)}`
   return (
-    <>
-      <div className="App-footer">
-        {user.isLoggedIn && (
-          <p>
-            <a href="#">Contact us</a>
-          </p>
-        )}
-        <p>
-          Copyright {getFullYear()} - {getFooterCopy()}
-        </p>
-      </div>
-    </>
-  );
+    <div className={css(styles.AppFooter)}>
+      <p className={css(styles.AppFooter_p)}>{footerText}</p>
+    </div>
+    )
+  }
+
+const mapStateToProps = (state) => {
+  const user = state.get("user")
+  const isLoggedIn = state.get("isUserLoggedIn")
+  return { user, isLoggedIn }
 }
 
-export default Footer;
+Footer.propTypes = {
+  user: PropTypes.shape({
+    email: PropTypes.string,
+    password: PropTypes.string
+  }),
+  isLoggedIn: PropTypes.bool
+}
+
+Footer.defaultProps = {
+    user: {
+      email: "",
+      password: ""
+    },
+    isLoggedIn: false
+}
+
+export default connect(mapStateToProps)(Footer)
