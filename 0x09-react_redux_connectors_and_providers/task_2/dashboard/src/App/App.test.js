f07578d7d6b59f1189aee0aabf84ef4566b2b51d
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-03-11 22:01:14 +0200

    Update App.test.js

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/App/App.test.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/App/App.test.js
@@ -20,8 +20,9 @@ afterEach(() => {
   jest.restoreAllMocks();
 });
 
-let wrapper = shallow(<App />);
+
 describe('App Component', () => {
+  let wrapper = shallow(<App />);
   it("renders without crashing", () => {
     expect(wrapper.exists()).toBe(true)
   })
@@ -42,50 +43,21 @@ describe('App Component', () => {
     expect(wrapper.containsMatchingElement(<CourseList/>)).toEqual(false)
   })
 
-  it("state is updated accordingly, after calling logIn", () => {
-    // let wrapper = shallow(<App />);
-    wrapper.instance().logIn("jack", "badpassword")
-
-    // user details are set and isLoggedIn is now set to true
-    expect(wrapper.state('user')).toEqual({"email": "jack", "isLoggedIn": true, "password": "badpassword"})
-  })
-
-  it("state is updated accordingly, after calling logOut", () => {
-
-    wrapper.instance().logOut()
-    // default user details are set with isLoggedIn set to false
-    expect(wrapper.state('user')).toEqual({"email": "", "isLoggedIn": false, "password": ""})
-  })
-
-  it("listNotifications state is updated accordingly, after calling markNotificationAsRead", () => {
-    // listNotifications state has a length of three
-    wrapper.setState({listNotifications: listNotifications})
-    wrapper.instance().markNotificationAsRead(1)
-    // listNotifications state now has a length of two
-    expect(wrapper.state('listNotifications').length).toEqual(2)
-  })
-
-  // it('alerts and calls func', () =>{
-  //   jest.spyOn(window, 'alert').mockImplementation(() => {});
-  //   const spy = jest.spyOn(App.prototype, 'alert')
-  //   const wrap = mount(<App logOut={()=>console.log('test')}/>)
-  //   // global.alert = jest.fn();
-  //   // console.log(App.prototype.alert())
-  //   // console.log(wrap.alert())
-  //   const event = new KeyboardEvent('keydown', {key:'h', ctrlKey: true})
-  //   console.log(window.dispatchEvent(event))
-    
-  //   expect(window.alert).toHaveBeenCalledTimes(1);
-  //   // console.log(wrap.html())
+  // it("listNotifications state is updated accordingly, after calling markNotificationAsRead", () => {
+  //   // listNotifications state has a length of three
+  //   wrapper.setState({listNotifications: listNotifications})
+  //   wrapper.instance().markNotificationAsRead(1)
+  //   // listNotifications state now has a length of two
+  //   expect(wrapper.state('listNotifications').length).toEqual(2)
   // })
-
 
 })
 
-// const wrapper_isLoggedIn = shallow(<App/>);
+
 describe('App Component when isLoggedin is true', () => {
+  const wrapper = shallow(<App isLoggedIn={true}/>);
+
   it("does not render Login component", () => {
-    wrapper.setState({user:{email:'', password:'', isLoggedIn:true}})
     expect(wrapper.containsMatchingElement(<Login/>)).toEqual(false)
   })
 
