792759b6bb9827ef594059677ba11560a49b3459
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-03-11 23:47:22 +0200

    Connect notifications to the reducer

--- 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/Notifications/Notifications.js
@@ -1,139 +1,159 @@
-import React, { Component, PureComponent } from "react";
-import { StyleSheet, css } from "aphrodite";
-import closeIcon from "../assets/close-icon.png";
-import NotificationItem from "./NotificationItem";
-import PropTypes from "prop-types";
-import NotificationItemShape from "./NotificationItemShape";
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import closebtn from '../assets/close-btn.png';
+import NotificationItem from './NotificationItem';
+import PropTypes from 'prop-types';
+import { fetchNotifications } from '../actions/notificationActionCreators';
+import { connect } from "react-redux"
 
-class Notifications extends PureComponent {
-  constructor(props) {
-    super(props);
+
+const bounceKeyFrames = {
+    "0%": {
+      transform: "translateY(-4px)",
+    },
+  
+    "100%": {
+      transform: "translateY(2px)"
+    }
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        {!this.props.displayDrawer ? (
-          <div
-            className={css(styles.menuItem)}
-            onClick={() => {
-              this.props.handleDisplayDrawer();
-            }}
-          >
-            <p>Your notifications</p>
-          </div>
-        ) : (
-          <div className={css(styles.Notifications)}>
-            <button
-              style={{
-                color: "#3a3a3a",
-                fontWeight: "bold",
-                background: "none",
-                border: "none",
-                fontSize: "15px",
-                position: "absolute",
-                right: "3px",
-                top: "3px",
-                cursor: "pointer",
-                outline: "none",
-              }}
-              aria-label="Close"
-              onClick={(e) => {
-                console.log("Close button has been clicked");
-                this.props.handleHideDrawer();
-              }}
-            >
-              <img src={closeIcon} alt="close icon" width="10px" />
-            </button>
-            {this.props.listNotifications.length != 0 ? <p>Here is the list of notifications</p> : null}
-            <ul>
-              {this.props.listNotifications.length == 0 ? <NotificationItem type="default" value="No new notification for now" /> : null}
-              {this.props.listNotifications.map((val, idx) => {
-                return <NotificationItem type={val.type} value={val.value} html={val.html} key={val.id} markAsRead={this.markNotificationAsRead} id={val.id} />;
-              })}
-            </ul>
-          </div>
-        )}
-      </React.Fragment>
-    );
-  }
+const opacityKeyFrames = {
+      "0%": {
+        opacity: "0.5",
+      },
+    
+      "100%": {
+        opacity: "1"
+      }
 }
 
-const opacityAnim = {
-  "0%": { opacity: 0.5 },
-  "100%": { opacity: 1 },
-};
-
-const bounceAnim = {
-  "0%": { transform: "translateY(0px)" },
-  "33%": { transform: "translateY(-5px)" },
-  "66%": { transform: "translateY(5px)" },
-  "100%": { transform: "translateY(0px)" },
-};
-
 const styles = StyleSheet.create({
-  Notifications: {
-    padding: "1em",
-    border: "2px dashed red",
+  Notifications:   {
+    background:"white",
+    border:"2px dashed red",
+    padding: 5,
+    width: "60%",
     position: "absolute",
-    top: "1.8em",
-    right: "0",
-    zIndex: "100",
-    "@media (max-width: 900px)": {
-      width: "100%",
-      padding: "0px",
-      fontSize: 20,
-      position: "relative",
-      right: 0,
-      left: 0,
+    right: 0,
+    top: "5%",
+    marginRight: 10,
+    '@media (max-width: 900px)': {
       border: "none",
-    },
+      top: 0,
+      height: "200%",
+      padding: 0,
+      fontSize: 20,
+      width: "97%",
+      background: "white",
+      zIndex: "9999"
+    }
   },
 
-  "notification-header": {
-    display: "flex",
-    justifyContent: "space-between",
-  },
-
-  menuItem: {
-    position: "relative",
-    zIndex: 100,
-    textAlign: "right",
-    ":hover": {
-      cursor: "pointer",
-      animationName: [opacityAnim, bounceAnim],
-      animationDuration: "1s, 0.5s",
-      animationIterationCount: "3",
-    },
+  p: {
+    marginTop: 0,
+    width: "100%"
   },
 
   ul: {
-    "@media (max-width: 900px)": {
+    margin: 1,
+    '@media (max-width: 900px)': {
       padding: 0,
-    },
+      }
   },
-  button: {
-    "@media (max-width: 900px)": {
-      position: "relative",
-      float: "right",
-    },
+
+  menuList: {
+    margin: 1
   },
-});
 
-Notifications.propTypes = {
-  displayDrawer: PropTypes.bool,
-  listNotifications: PropTypes.arrayOf(NotificationItemShape),
-  handleDisplayDrawer: PropTypes.func,
-  handleHideDrawer: PropTypes.func,
-  markNotificationAsRead: PropTypes.func,
-};
+  menuItem: {
+    position: "fixed",
+    right: 0,
+    paddingRight: 10,
+    marginBottom: 6,
+    ":hover": {
+      animationName: [bounceKeyFrames, opacityKeyFrames],
+      animationDuration: "0.5s, 1s",
+      animationIterationCount: 3,
+      cursor: "pointer"
+    }
+  },
 
-Notifications.defaultProps = {
-  displayDrawer: false,
-  listNotifications: [],
-  handleDisplayDrawer: () => {},
-  handleHideDrawer: () => {},
-  markNotificationAsRead: () => {},
-};
+  hideMenuItem: {
+    display:"none"
+  }
+})
 
-export default Notifications;
+
+export class Notifications extends React.PureComponent {
+  static propTypes = {
+    messages: PropTypes.object,
+  }
+
+  static defaultProps = {
+      messages: {},
+      fetchNotifications: () => {}
+  }
+
+  componentDidMount() {
+    this.props.fetchNotifications()
+  }
+
+  render() {
+    const loadNotifs = () => {
+      let rows = <></>
+      const notifArray = Object.values(this.props.messages)
+      // console.log(notifArray)
+      if (notifArray.length == 0){
+          return <p>No new notification for now</p>
+      } else {
+          rows = notifArray.map((notif, key) => {
+            return (<NotificationItem key={key} id={notif.guid} type={notif.type}
+            value={notif.value} markNotificationAsRead={this.props.markNotificationAsRead}/>)
+          })
+      }
+      return (
+        <>
+        <p className={css(styles.p)}>Here is the list of notifications:</p>
+            <ul className={css(styles.ul)}>
+              {rows}
+            </ul>
+        </>
+      )
+    }
+    const showNotifs = () => {
+      if (this.props.displayDrawer) {
+        return (
+          <>
+            <div className={css(styles.Notifications)}>
+              <button style={{float:'right', background: 'none', border: 'none'}}
+              id="close-btn"
+              aria-label="Close"
+              onClick={this.props.hideDrawer}>
+                <img src={closebtn} alt="close-btn"/>
+              </button>
+              {loadNotifs()}
+            </div>
+          </>
+        )
+      }
+    }
+    return (
+    <>
+      <div onClick={this.props.showDrawer} className={css(this.props.displayDrawer ? styles.hideMenuItem : styles.menuItem)}>Your notifications</div>
+      {showNotifs()}
+    </>
+    )
+  }
+}
+
+const mapPropstoState = (state) =>{
+  return { messages: state.notifications.get("notifications") }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchNotifications: () => dispatch(fetchNotifications())
+  }
+}
+
+export default connect(mapPropstoState, mapDispatchToProps)(Notifications)
