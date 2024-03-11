4433da03868c511ab607a034baafe2ec55c92814
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-03-11 21:59:55 +0200

    Update Footer.test.js

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.test.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.test.js
@@ -1,65 +1,31 @@
-/**
- * @jest-environment jsdom
- */
-import { shallow, mount } from "enzyme";
-import React from "react";
-import Footer from "./Footer";
-import { getFullYear, getFooterCopy } from "../utils/utils";
-import { AppContext } from "../App/AppContext";
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Footer } from './Footer';
+import { StyleSheetTestUtils } from 'aphrodite';
 
-describe("Footer test", () => {
-  it("should render without crashing", () => {
-    const wrapper = shallow(<Footer />);
-    expect(wrapper.exists()).toEqual(true);
-  });
-
-  it('Footer component renders "Copyright ${getFullYear()} - ${getFooterCopy(true)}"', () => {
-    const wrapper = mount(<Footer />);
-
-    expect(wrapper.find("p").text()).toEqual(`Copyright ${getFullYear()} - ${getFooterCopy(false)}`);
-  });
-
-  it("Tests that there is no link rendered when user is logged out within context", () => {
-    const context = {
-      user: {
-        email: "",
-        password: "",
-        isLoggedIn: false,
-      },
-    };
-
-    const wrapper = mount(
-      <AppContext.Provider value={context}>
-        <Footer />
-      </AppContext.Provider>
-    );
-
-    expect(wrapper.find("a").length).toBe(0);
-    expect(wrapper.find("a").exists()).toBe(false);
-    expect(wrapper.text()).not.toContain("Contact us");
-
-    wrapper.unmount();
-  });
-
-  it("Tests that there is a link rendered when user is logged in within context", () => {
-    const context = {
-      user: {
-        email: "",
-        password: "",
-        isLoggedIn: true,
-      },
-    };
-
-    const wrapper = mount(
-      <AppContext.Provider value={context}>
-        <Footer />
-      </AppContext.Provider>
-    );
-
-    expect(wrapper.find("a").length).toBe(1);
-    expect(wrapper.find("a").exists()).toBe(true);
-    expect(wrapper.text()).toContain("Contact us");
-
-    wrapper.unmount();
-  });
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
 });
+
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection()
+});
+
+
+const wrapper = shallow(<Footer/>)
+describe('Footer component', () => {
+  it('renders without crashing', () => {
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('renders the text “Copyright” when context is set to:(user defined, isLoggedIn is false and an email is set)', () => {
+    const wrapper = shallow(<Footer/>)
+    expect(wrapper.find('p').text()).toContain("Copyright");
+  })
+
+  it('renders the text "Contact us" when context is set to:( user defined, isLoggedIn is true and an email is set)', () => {
+    defaultUser.isLoggedIn = true
+    const wrapper = shallow(<Footer isLoggedIn={true}/>)
+    expect(wrapper.find('p').text()).toContain("Contact us");
+  })
+})
