ef58939f66aadc9d8ad1d8103ba566ba5ecbb852
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-03-11 21:57:02 +0200

    Modify the uiReducer

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
@@ -1,10 +1,6 @@
 import { Map } from "immutable";
-import { LOGOUT, DISPLAY_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE, HIDE_NOTIFICATION_DRAWER } from '../actions/uiActionTypes'
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE, HIDE_NOTIFICATION_DRAWER } from '../actions/uiActionTypes'
 
-// Install Immutable.js within the project
-// Update the uiReducer.js file to use Map from Immutable.js
-// Update the different part of the reducer function to use set from Map
-// Update the test suite, so it takes into account the changes
 
 export const initialState = Map({
   isNotificationDrawerVisible: false,
@@ -18,11 +14,15 @@ export function uiReducer(state = initialState, action) {
       return state.set("isNotificationDrawerVisible", true)
     case HIDE_NOTIFICATION_DRAWER:
       return state.set("isNotificationDrawerVisible", false)
+    case LOGIN:
+      return state.set("user", action.user)
     case LOGIN_SUCCESS:
       return state.set("isUserLoggedIn", true)
-    case LOGIN_FAILURE: 
-    case LOGOUT:
+    case LOGIN_FAILURE:
       return state.set("isUserLoggedIn", false)
+    case LOGOUT: {
+      return state.withMutations((state)=>state.set("isUserLoggedIn", false).set("user", null))
+    }
     default:
       return state
   }
